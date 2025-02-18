8f629d342ad74d6d7aeb414466fe43d05fdc60e9
Alexander Victor Gronau <alex.gronau.5@gmail.com>
2025-02-18 16:18:27 +0100

    production setup for static files

--- ambrosia/ambrosia/settings.py
+++ ambrosia/ambrosia/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / ''
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
