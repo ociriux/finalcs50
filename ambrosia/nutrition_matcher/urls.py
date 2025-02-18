87451677b1c2d77763c203ad6c5ca547a1e36749
Alexander Victor Gronau <alex.gronau.5@gmail.com>
2025-02-18 23:37:07 +0100

    experiment with the USDA FoodData Central API

--- ambrosia/nutrition_matcher/urls.py
+++ ambrosia/nutrition_matcher/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('food/', views.food, name='food'),
     path('food/details/<int:id>', views.details, name='details'),
+    path('usda/', views.usda, name='usda')
 ]
--- ambrosia/nutrition_matcher/views.py
+++ ambrosia/nutrition_matcher/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Food
+import requests
 
 def index(request):
     template = loader.get_template('index.html')
@@ -25,3 +26,21 @@ def details(request, id):
         'food' : foodItem,
     }
     return HttpResponse(template.render(context, request))
+
+
+def usda(request):
+    api_key = 'gdeU5YEXk67OSPqgivqM1SU4ltoiozPa5tsADXfJ'
+    query = request.GET.get('query', 'apple')  # Standardabfrage 'apple', falls keine Abfrage gestellt wird
+    url = 'https://api.nal.usda.gov/fdc/v1/foods/search'
+    params = {
+        'api_key': api_key,
+        'query': query,
+        'pageSize': 10  # Anzahl der zurückgegebenen Ergebnisse
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        foods = data.get('foods', [])
+    else:
+        foods = []
+    return render(request, 'usda.html', {'foods': foods, 'query': query})
