d986f93566a40770e539ec31d2b37f20567919f7
Alexander Victor Gronau <95646757+ociriux@users.noreply.github.com>
2025-02-21 23:47:20 +0000

    updating model structure

--- ambrosia/usda_sync/models.py
+++ ambrosia/usda_sync/models.py
@@ -1,8 +1,32 @@
 from django.db import models
 
 class Food(models.Model):
-        food_id = models.IntegerField(primary_key=True)
+        id = models.IntegerField(primary_key=True)
         description = models.CharField(max_length=255)
+        category = models.CharField(max_length=255)
+        calorie_conversion_factor_protein = models.FloatField(null=True)
+        calorie_conversion_factor_fat = models.FloatField(null=True)
+        calorie_conversion_factor_carbs = models.FloatField(null=True)
+
+        def __str__(self):
+                return self.description
 
 class Nutrient(models.Model):
-        nutrient_id = models.IntegerField(primary_key=True)
+        id = models.IntegerField(primary_key=True)
+        description = models.CharField(max_length=255)
+        category = models.CharField(max_length=255)
+        unit = models.CharField(max_length=255)
+
+        def __str__(self):
+                return self.description
+        
+class FoodNutrient(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
+    class Meta:
+        unique_together = ('food', 'nutrient')
+
+    def __str__(self):
+        return f"{self.food} - {self.nutrient}: {self.amount} {self.nutrient.unit}"
