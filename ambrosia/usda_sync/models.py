ca27c6240aea832898c6678ee1937d611445744c
Alexander Victor Gronau <alex.gronau.5@gmail.com>
2025-02-21 18:08:31 +0100

    create models for usda_sync

--- ambrosia/usda_sync/models.py
+++ ambrosia/usda_sync/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+        food_id = models.IntegerField(primary_key=True)
+        description = models.CharField(max_length=255)
+
+class Nutrient(models.Model):
+        nutrient_id = models.IntegerField(primary_key=True)
