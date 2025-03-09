5b33f26decb62fb685f3e6eebc299e291015b8ba
Alexander Victor Gronau <alex.gronau.5@gmail.com>
2025-03-09 12:40:36 +0100

    Add decouple for sensitive data

--- ambrosia/ambrosia/settings.py
+++ ambrosia/ambrosia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ijt#n#-4e6y0njuz&!1_#y)p*m2cyn#h)bw93(*7m**39p_jc4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
